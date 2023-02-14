599b5ed12dcf889db2b3e97bd8a94e4e113ae53a
Артем К <artush00@yandex.ru>
2023-02-14 06:13:03 +0400

    add url and view

---
+++ yatube/posts/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse('Главная страница')
+
+def group_posts(request, slug):
+    return HttpResponse(f'Список {slug}')
+# Create your views here.
