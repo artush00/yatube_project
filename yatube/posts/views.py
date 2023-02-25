2dff329c73ec788cfcc54a165bb4903fe6fe3fd6
Артем К <artush00@yandex.ru>
2023-02-25 21:02:40 +0400

    1

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(request):
-    return HttpResponse('Главная страница')
+
 
 def group_posts(request, slug):
     return HttpResponse(f'Список {slug}')
 # Create your views here.
+
+def index(request):
+    template = 'posts/index.html'
+    return render(request, template) 
